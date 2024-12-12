40ca27d46b5d3f5e2a7b21c399b296c569120094
amitmisal24 <113032155+amitmisal24@users.noreply.github.com>
2024-12-12 19:08:24 +0530

    Update Project.js

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Filesharing from "../assest/img/Filesharing.png"
+import Crud from "../assest/img/crud.png"
 import ProjectCard from './ProjectCard';
 
 const Project = () => {
@@ -8,6 +9,7 @@ const Project = () => {
           <h1 className='heading text-center'>Project</h1>
           <div className='project_content'>
             <ProjectCard name="File Sharing" url="https://github.com/amitmisal24/file-sharing-app" img={Filesharing}/>
+             <ProjectCard name="Crud" url="https://github.com/amitmisal24/file-sharing-app" img={Crud}/>
           </div>
         </div>
   )
